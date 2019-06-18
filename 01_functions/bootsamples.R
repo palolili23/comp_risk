c7bb01db6a69808bbed9bdebbca169e5d924c2a4
Paloma <palolili@gmail.com>
2019-06-18 09:59:32 +0200

    Added the parameters that specify the model for Y, C and D

--- 01_functions/bootsamples.R
+++ 01_functions/bootsamples.R
@@ -1,10 +1,23 @@
-bootsamples <- function(data, n, factors, time_fx, surv_model) {
-  set.seed(123)
+bootsamples <- function(data, n, 
+                        seed = 123,
+                        factors_outcome,
+                        factors_cens,
+                        factors_cr,
+                        rows = max(data$max),
+                        surv_model){
+  # Set seed
+  set.seed(seed)
+  
+  # Creates bootsamples and runs the model to each sample
   bootsamps <- replicate(n = n, expr = {
     d <- sample(1:nrow(data),size = nrow(data), replace = T)
     ds_b <- data[d,] %>%
-      mutate(id = row_number())
-    return(surv_model(ds_b, factors, time_fx))
+    mutate(id = row_number()) 
+    return(surv_model(ds_b, 
+                      factors_outcome,
+                      factors_cens,
+                      factors_cr,
+                      rows))
   }, simplify = F)
   
   samples <- as.list(c(1:n))
