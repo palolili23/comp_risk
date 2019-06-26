1dfe0db185b62f27ab23e258ce2b209a7040fdaf
Paloma RS <l.rojassaunero@erasmusmc.nl>
2019-06-26 18:29:50 +0200

    Total effect with gformula

---
+++ 01_functions/37_bootsamples.R
@@ -0,0 +1,27 @@
+bootsamples37 <- function(data, n, 
+                          seed = 123,
+                          factors_outcome,
+                          factors_cr,
+                          rows = max(data$max)){
+  # Set seed
+  set.seed(seed)
+  
+  # Creates bootsamples and runs the model to each sample
+  bootsamps <- replicate(n = n, expr = {
+    d <- sample(1:nrow(data),size = nrow(data), replace = T)
+    ds_b <- data[d,] %>%
+      mutate(id = row_number()) 
+    return(total_gf_pr(ds_b, 
+                          factors_outcome,
+                          factors_cr,
+                          rows))
+  }, simplify = F)
+  
+  samples <- as.list(c(1:n))
+  
+  boot_bind <- Map(cbind, bootsamps, sample = samples)
+  totalboot <- reduce(boot_bind, bind_rows)
+  
+  return(totalboot)
+}
+
