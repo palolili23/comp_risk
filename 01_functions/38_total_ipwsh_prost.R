a9589c562a55db8be2c086b041dbb79837f40eda
Paloma RS <l.rojassaunero@erasmusmc.nl>
2019-07-30 12:14:15 +0200

    No need to convert to NA since it's a different data setup

--- 01_functions/38_total_ipwsh_prost.R
+++ 01_functions/38_total_ipwsh_prost.R
@@ -25,14 +25,6 @@ total_ipwsh_pr <- function(data,
     ungroup() %>%
     arrange(id, time)
   
-  data_long %<>% 
-    mutate(
-      outcome_plr = case_when(
-        cens_plr == 1 ~ NA_real_,
-        competing_plr == 1 ~ NA_real_,
-        TRUE ~ outcome_plr),
-      competing_plr = ifelse(cens_plr == 1, NA, competing_plr))
-  
   # Create weights ----------------------------------------------------------
   model_denom_cens <-
     reformulate(termlabels = factors_cens, response = "no_cens")
