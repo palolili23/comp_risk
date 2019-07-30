c391775a4e75244bf950508ba98c4a21e9e7e7eb
Paloma RS <l.rojassaunero@erasmusmc.nl>
2019-07-30 10:52:39 +0200

    Works!!!

--- 01_functions/37_total_gf_prost.R
+++ 01_functions/37_total_gf_prost.R
@@ -3,7 +3,7 @@ total_gf_pr <- function(data,
                         factors_cr,
                         rows = max(data$max)) {
   #transform from wide to long and create necessary variables
-  n_expanding_rows <- rows + 1
+  n_expanding_rows <- rows
   
   data_long <- data[rep(seq(nrow(data)), n_expanding_rows), ]
   
@@ -13,14 +13,20 @@ total_gf_pr <- function(data,
       time = row_number() - 1,
       outcome_plr = ifelse(time == max, outcome, 0),
       competing_plr = ifelse(time == max, competing, 0),
-      no_cr = 1 - competing_plr,
-      cens_plr = ifelse(time == max, cens, 0),
-      no_cens = 1 - cens
+      cens_plr = ifelse(time == max, cens, 0)
     ) %>%
     filter(time <= max) %>%
     ungroup() %>%
     arrange(id, time)
   
+  data_long %<>% 
+    mutate(
+      outcome_plr = case_when(
+        cens_plr == 1 ~ NA_real_,
+        competing_plr == 1 ~ NA_real_,
+        TRUE ~ outcome_plr),
+      competing_plr = ifelse(cens_plr == 1, NA, competing_plr))
+
   #fit the model
   model_y <-
     reformulate(termlabels = factors_outcome, response = "outcome_plr")
@@ -31,7 +37,7 @@ total_gf_pr <- function(data,
   adj_plr_cr <- glm(model_cr, data = data_long, family = binomial)
   
   #create clones
-  data0 <- data1 <- data[rep(seq(nrow(data)), n_expanding_rows), ]
+  data0 <- data1 <- data[rep(seq(nrow(data)), n_expanding_rows),]
   
   #predict probabilities when exposure = 0
   data0 %<>%
@@ -51,12 +57,12 @@ total_gf_pr <- function(data,
       s = (1 - py) * (1 - pd),
       cum_s = cumprod(s),
       s_lag = lag(cum_s),
-      cif = pd * s_lag,
-      cif = ifelse(is.na(cif), pd, cif),
+      cif = py * (1 - pd) * s_lag, 
+      cif = ifelse(is.na(cif), (py * (1 - pd)), cif), #This is for time 0
       cif_cum = cumsum(cif)) %>%
     ungroup()
+
     #predict probabilities when exposure = 1
-  
   data1 %<>%
     group_by(id) %>%
     mutate(exposure = 1,
@@ -74,8 +80,8 @@ total_gf_pr <- function(data,
       s = (1 - py) * (1 - pd),
       cum_s = cumprod(s),
       s_lag = lag(cum_s),
-      cif = pd * s_lag,
-      cif = ifelse(is.na(cif), pd, cif),
+      cif = py * (1 - pd) * s_lag,
+      cif = ifelse(is.na(cif), (py * (1 - pd)), cif),
       cif_cum = cumsum(cif)) %>%
     ungroup()
   
