89b9235a47a2d91766cf09185979d915037dc9c7
Paloma RS <l.rojassaunero@erasmusmc.nl>
2019-07-31 17:16:02 +0200

    update code

--- 02_R/steps to compare to mats.R
+++ 02_R/steps to compare to mats.R
@@ -12,7 +12,22 @@ treated <- treated %>% group_by(patno) %>% mutate(s_lag = lag(s_2)) %>% ungroup(
 treated <- treated %>% mutate(cuminc = hazardO * s_lag)
 treated <- treated %>% mutate(cuminc = ifelse(is.na(cuminc), hazardO, cuminc))
 
-cuminc2 <- treated %>% pull(cuminc)
+cuminc2 <- data1 %>% pull(cif)
 cuminc2 <- matrix(cuminc2, nrow= 60, ncol = 252)
 
 max(cumsum(cumulativeIncidence == cuminc2))
+
+cumprod2 <- data1 %>% pull(cif_cum)
+cumprod2 <- matrix(cumprod2, nrow= 60, ncol = 252)
+
+test2 <- apply(cuminc2, MARGIN = 2, cumsum)
+
+total <-
+  rowMeans(apply(cuminc2, MARGIN = 2, cumsum))
+
+View(total)
+
+max(cumsum(test2 == cumprod2))
+
+
+cumMats <- import("cumMats.csv")
