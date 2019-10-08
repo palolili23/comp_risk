962bb41bd4e84c1c42e1595ec9499cc083f0409b
Paloma <palolili@gmail.com>
2019-10-08 13:04:30 +0200

    changed gform fx

--- 01_functions/gformula.R
+++ 01_functions/gformula.R
@@ -1,9 +1,9 @@
-total_gf_pr <- function(data,
-                        factors_outcome,
-                        factors_cr,
-                        rows = max(data$max)) {
+gformula <- function(data,
+                         factors_outcome,
+                         factors_cr = NULL,
+                         rows = max(data$max)) {
   #transform from wide to long and create necessary variables
-
+  
   data_long <- data[rep(seq(nrow(data)), rows), ]
   
   data_long %<>%
@@ -25,15 +25,16 @@ total_gf_pr <- function(data,
         competing_plr == 1 ~ NA_real_,
         TRUE ~ outcome_plr),
       competing_plr = ifelse(cens_plr == 1, NA, competing_plr))
-
   #fit the model
   model_y <-
     reformulate(termlabels = factors_outcome, response = "outcome_plr")
   adj_plr_y <- glm(model_y, data = data_long, family = binomial)
   
-  model_cr <-
-    reformulate(termlabels = factors_cr, response = "competing_plr")
-  adj_plr_cr <- glm(model_cr, data = data_long, family = binomial)
+  if (!is.null(factors_cr)) {
+    model_cr <-
+      reformulate(termlabels = factors_cr, response = "competing_plr")
+    adj_plr_cr <- glm(model_cr, data = data_long, family = binomial)
+  }
   
   #create clones
   data0 <- data1 <- data[rep(seq(nrow(data)), rows),]
@@ -46,33 +47,17 @@ total_gf_pr <- function(data,
     ungroup()
   
   data0 %<>%
-    mutate(
-      py = predict(adj_plr_y, newdata = data0, type = "response"),
-      pd = predict(adj_plr_cr, newdata = data0, type = "response")
-    ) %>%
-    arrange(id, time) %>%
-    group_by(id) %>%
-    mutate(
-      s = (1 - py) * (1 - pd),
-      cum_s = cumprod(s),
-      s_lag = lag(cum_s),
-      cif = py * (1 - pd) * s_lag, 
-      cif = ifelse(is.na(cif), (py * (1 - pd)), cif), #This is for time 0
-      cif_cum = cumsum(cif)) %>%
-    ungroup()
-
-    #predict probabilities when exposure = 1
-  data1 %<>%
-    group_by(id) %>%
-    mutate(exposure = 1,
-           time = (row_number() - 1)) %>%
-    ungroup()
+    mutate(py = predict(adj_plr_y, newdata = data0, type = "response"))
   
-  data1 %<>%
-    mutate(
-      py = predict(adj_plr_y, newdata = data1, type = "response"),
-      pd = predict(adj_plr_cr, newdata = data1, type = "response")
-    ) %>%
+  if (!is.null(factors_cr)) {
+    data0 %<>%
+      mutate(pd = predict(adj_plr_cr, newdata = data0, type = "response"))
+  } else{
+    data0 %<>%
+      mutate(pd = 0)
+  }
+  
+  data0 %<>%
     arrange(id, time) %>%
     group_by(id) %>%
     mutate(
@@ -81,7 +66,40 @@ total_gf_pr <- function(data,
       s_lag = lag(cum_s),
       cif = py * (1 - pd) * s_lag,
       cif = ifelse(is.na(cif), (py * (1 - pd)), cif),
-      cif_cum = cumsum(cif)) %>%
+      #This is for time 0
+      cif_cum = cumsum(cif)
+    ) %>%
+    ungroup()
+  
+  #predict probabilities when exposure = 1
+  data1 %<>%
+    group_by(id) %>%
+    mutate(exposure = 1,
+           time = (row_number() - 1)) %>%
+    ungroup()
+  
+  data1 %<>%
+    mutate(py = predict(adj_plr_y, newdata = data1, type = "response"))
+  
+  if (!is.null(factors_cr)) {
+    data1 %<>%
+      mutate(pd = predict(adj_plr_cr, newdata = data1, type = "response"))
+  } else{
+    data1 %<>%
+      mutate(pd = 0)
+  }
+  
+  data1 %<>%
+    arrange(id, time) %>%
+    group_by(id) %>%
+    mutate(
+      s = (1 - py) * (1 - pd),
+      cum_s = cumprod(s),
+      s_lag = lag(cum_s),
+      cif = py * (1 - pd) * s_lag,
+      cif = ifelse(is.na(cif), (py * (1 - pd)), cif),
+      cif_cum = cumsum(cif)
+    ) %>%
     ungroup()
   
   #combine sets and estimate the mean_survival
--- 01_functions/gformula_boot.R
+++ 01_functions/gformula_boot.R
@@ -1,20 +1,20 @@
-bootsamples37 <- function(data, n, 
+gformula_boot <- function(data, n, 
                           seed = 123,
                           factors_outcome,
-                          factors_cr,
+                          factors_cr = NULL,
                           rows = max(data$max)){
   # Set seed
   set.seed(seed)
   
   # Creates bootsamples and runs the model to each sample
   bootsamps <- replicate(n = n, expr = {
-    d <- sample(1:nrow(data),size = nrow(data), replace = T)
-    ds_b <- data[d,] %>%
-      mutate(id = row_number()) 
-    return(total_gf_pr(ds_b, 
-                          factors_outcome,
-                          factors_cr,
-                          rows))
+    d <- sample(1:nrow(data), size = nrow(data), replace = T)
+    ds_b <- data[d, ] %>%
+      mutate(id = row_number())
+    return(gformula(ds_b,
+                        factors_outcome,
+                        factors_cr,
+                        rows))
   }, simplify = F)
   
   samples <- as.list(c(1:n))
